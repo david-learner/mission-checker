760f40f74d5e2009a7059f61ddfb415d078e8732
david <xodnjs2549@naver.com>
2022-02-23 20:49:45 +0900

    Make Project

---
+++ src/test/java/com/missionchecker/MissionCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.missionchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MissionCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
