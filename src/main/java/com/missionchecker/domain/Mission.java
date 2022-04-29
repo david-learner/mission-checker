7633c39f6f345c8fca5560bef4038cbd630075a0
david <xodnjs2549@naver.com>
2022-04-29 10:58:21 +0900

    feat(mission): implement mission applying and accepting

--- src/main/java/com/missionchecker/domain/Mission.java
+++ src/main/java/com/missionchecker/domain/Mission.java
@@ -1,10 +1,21 @@
 package com.missionchecker.domain;
 
-import lombok.Getter;
-
-import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import lombok.Getter;
 
 @Entity
 @Table(name = "MISSION")
@@ -17,15 +28,21 @@ public class Mission {
     private String name;
     @OneToOne
     private Member creator;
+    @OneToMany(mappedBy = "mission", cascade = CascadeType.ALL)
+    private List<Check> checks;
+    // 미션 신청 정보
+    @OneToMany(mappedBy = "mission", cascade = CascadeType.ALL)
+    private Set<Application> applications;
     // 미션 참여자 정보
     @OneToMany(mappedBy = "mission", cascade = CascadeType.ALL)
     private Set<Participation> participations;
-    @OneToMany(mappedBy = "mission", cascade = CascadeType.ALL)
-    private List<Check> checks;
     // 미션 관리자 정보
     @OneToMany(mappedBy = "mission", cascade = CascadeType.ALL)
     private Set<Administration> administrations;
+    // todo boolean to bit converter 적용
+    @Column(columnDefinition = "boolean default false")
     private Boolean isClosed = Boolean.FALSE;
+    @Column(columnDefinition = "boolean default false")
     private Boolean isDeleted = Boolean.FALSE;
     private LocalDate openDate;
     private LocalDate closeDate;
@@ -33,9 +50,11 @@ public class Mission {
     public Mission() {
     }
 
-    private Mission(Member creator, String name, Set<Participation> participations, List<Check> checks, Set<Administration> administrations, LocalDate openDate, LocalDate closeDate) {
+    private Mission(Member creator, String name, Set<Application> applications, Set<Participation> participations,
+                    List<Check> checks, Set<Administration> administrations, LocalDate openDate, LocalDate closeDate) {
         this.creator = creator;
         this.name = name;
+        this.applications = applications;
         this.participations = participations;
         this.checks = checks;
         this.administrations = administrations;
@@ -45,6 +64,7 @@ public class Mission {
 
     /**
      * 미션을 생성한다. 미션을 생성한 사람은 자동으로 미션의 관리자로 추가된다.
+     *
      * @param creator
      * @param missionName
      * @return
@@ -52,14 +72,30 @@ public class Mission {
     public static Mission of(Member creator, String missionName) {
         // 초기화
         Set<Participation> participations = new HashSet<>();
+        Set<Application> applications = new HashSet<>();
         List<Check> checks = new ArrayList<>();
         Set<Administration> administrations = new HashSet<>();
         LocalDate now = LocalDate.now();
 
         // 생성
-        Mission mission = new Mission(creator, missionName, participations, checks, administrations, now, now);
+        Mission mission = new Mission(creator, missionName, applications, participations, checks, administrations, now,
+                now);
         mission.addAdministration(new Administration(creator, mission));
         return mission;
+    }
+
+    public void addApplicant(Member applicant) {
+        // todo set 중복 삽입시 발생하는 예외 어떻게 처리해줄건지
+        Application newApplication = new Application(applicant, this);
+        if(!applications.contains(newApplication)) {
+            applications.add(newApplication);
+            return;
+        }
+        throw new IllegalArgumentException("동일한 미션에 중복신청할 수 없습니다");
+    }
+    
+    public boolean isParticipating(Member participant) {
+        return participations.contains(new Participation(participant, this));
     }
 
     public void addParticipant(Member participant) {
@@ -71,22 +107,6 @@ public class Mission {
         participant.addParticipation(participation);
     }
 
-    public List<Check> getAllChecksBy(Member administrator) {
-        if (isAdministrator(administrator)) {
-            return checks;
-        }
-        throw new IllegalArgumentException("해당 미션 관리자가 아닙니다");
-    }
-
-    private boolean isAdministrator(Member administrator) {
-        for (Administration administration : administrations) {
-            if (administration.isAdministrator(administrator)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     // 미션 생성할 때 생성자의 관리자 정보 목록에 관리자 정보 추가하기
     public void addAdministration(Administration administration) {
         // Mission의 Administration
@@ -95,16 +115,45 @@ public class Mission {
         administration.getMember().addAdministration(administration);
     }
 
+    public void acceptApplyingRequest(Member applicant) {
+        try {
+            applications.remove(applicant);
+            participations.add(new Participation(applicant, this));
+        } catch (Exception e) {
+            // todo rollback
+        }
+    }
+
+    public List<Check> getAllChecksBy(Member administrator) {
+        if (isAdministrator(administrator)) {
+            return checks;
+        }
+        throw new IllegalArgumentException("해당 미션 관리자가 아닙니다");
+    }
+
+    public boolean isAdministrator(Member administrator) {
+        for (Administration administration : administrations) {
+            if (administration.isAdministrator(administrator)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Mission mission = (Mission) o;
-        return Objects.equals(name, mission.name) && Objects.equals(openDate, mission.openDate) && Objects.equals(closeDate, mission.closeDate);
+        return Objects.equals(creator, mission.creator) && Objects.equals(name, mission.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, openDate, closeDate);
+        return Objects.hash(creator, name);
     }
 }
--- src/main/java/com/missionchecker/web/MissionController.java
+++ src/main/java/com/missionchecker/web/MissionController.java
@@ -3,15 +3,17 @@ package com.missionchecker.web;
 import com.missionchecker.domain.Member;
 import com.missionchecker.domain.Mission;
 import com.missionchecker.dto.MissionCreationRequest;
-import com.missionchecker.repository.MissionRepository;
 import com.missionchecker.service.MissionService;
+import com.missionchecker.support.SessionMember;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
@@ -19,8 +21,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 @RequiredArgsConstructor
 public class MissionController {
 
-    private final MissionRepository missionRepository;
     private final MissionService missionService;
+
+    @GetMapping("/missions")
+    public String missions(SessionMember loginMember, Model model) {
+        List<Mission> missionsCreatedByMember = missionService.findByAllMissionsCreatedBy(loginMember);
+//        List<Mission> participatingMissions = missionService.findByAllParticipatingMissions(loginMember);
+        model.addAttribute("missionsCreatedByMember", missionsCreatedByMember);
+//        model.addAttribute("participatingMissions", participatingMissions);
+        return "mission/home";
+    }
+
+    @PostMapping("/missions")
+    public String createMission(MissionCreationRequest creationRequest, SessionMember loginMember) {
+        missionService.createMission(creationRequest, loginMember);
+        return "redirect:/missions/completion";
+    }
 
     @GetMapping("/missions/registration-form")
     public String missionRegistrationForm(Model model) {
@@ -28,22 +44,20 @@ public class MissionController {
         return "mission/registration-form";
     }
 
-    @GetMapping("/missions")
-    public String missions(Member loginMember, Model model) {
-        List<Mission> missions = missionService.findByAllMissionsCreatedBy(loginMember);
-        model.addAttribute("missions", missions);
-        return "mission/home";
-    }
-
-    @PostMapping("/missions")
-    public String createMission(MissionCreationRequest creationRequest, Member loginMember) {
-        missionRepository.save(creationRequest.toMission(loginMember));
-        return "redirect:/missions/completion";
-    }
-
     @GetMapping("/missions/completion")
     public String completeOpeningMission(Member loginMember) {
-        log.debug(loginMember.getName());
         return "mission/completion";
+    }
+
+    @GetMapping("/missions/{missionId}")
+    public String missionDetailForm(@PathVariable Long missionId, Model model) {
+        model.addAttribute("mission", missionService.findOneById(missionId));
+        return "mission/detail";
+    }
+
+    @PostMapping("/missions/{missionId}/apply")
+    public ResponseEntity applyMission(@PathVariable Long missionId, SessionMember loginMember, Model model) {
+        missionService.applyMission(loginMember, missionId);
+        return ResponseEntity.ok().build();
     }
 }
