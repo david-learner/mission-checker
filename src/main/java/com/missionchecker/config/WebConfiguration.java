8be8dedb36164ad1153c02495f03dbea48d410c2
david <xodnjs2549@naver.com>
2022-04-29 10:54:18 +0900

    fix(configuration): remove dependency for MemberRepository

--- src/main/java/com/missionchecker/config/WebConfiguration.java
+++ src/main/java/com/missionchecker/config/WebConfiguration.java
@@ -1,34 +1,25 @@
 package com.missionchecker.config;
 
-import com.missionchecker.repository.MemberRepository;
 import com.missionchecker.support.LoginFilter;
 import com.missionchecker.support.LoginMemberHandlerMethodArgumentResolver;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
-
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
-    private final MemberRepository memberRepository;
-
-    @Autowired
-    public WebConfiguration(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(new LoginMemberHandlerMethodArgumentResolver(memberRepository));
+        resolvers.add(new LoginMemberHandlerMethodArgumentResolver());
     }
 
     /**
      * 로그인 필터 등록
+     *
      * @return
      */
     @Bean
