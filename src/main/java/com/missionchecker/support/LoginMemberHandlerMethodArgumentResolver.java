014de22ed69b0298f6a6e611d7bf2c7f869a1ee5
david <xodnjs2549@naver.com>
2022-04-29 10:52:48 +0900

    fix(login-resolver): change support type Member to SessionMember

--- src/main/java/com/missionchecker/support/LoginMemberHandlerMethodArgumentResolver.java
+++ src/main/java/com/missionchecker/support/LoginMemberHandlerMethodArgumentResolver.java
@@ -1,6 +1,5 @@
 package com.missionchecker.support;
 
-import com.missionchecker.domain.Member;
 import com.missionchecker.repository.MemberRepository;
 import com.missionchecker.util.Constant;
 import org.springframework.core.MethodParameter;
@@ -12,35 +11,31 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 /**
- * 로그인한 사용자의 경우 세션에 있는 사용자 정보를 꺼내어 핸들러 매개변수에 넣어준다.
- * 핸들러 매개변수로 타입은 SessionMember, 이름은 loginMember여야 동작한다.
+ * 로그인한 사용자의 경우 세션에 있는 사용자 정보를 꺼내어 핸들러 매개변수에 넣어준다. 핸들러 매개변수로 타입은 SessionMember, 이름은 loginMember여야 동작한다.
  */
 @Component
 public class LoginMemberHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
 
-    public static final String LOGIN_MEMBER = "loginMember";
-    private final MemberRepository memberRepository;
-
-    public LoginMemberHandlerMethodArgumentResolver(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
+    public static final String NOT_EXISTED_LOGIN_MEMBER = "세션 내 로그인 사용자 정보가 존재하지 않습니다.";
+    public static final String HANDLER_LOGIN_MEMBER_ARGUMENT_NAME = "loginMember";
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        if(parameter.getParameterType().equals(Member.class) &&
-                parameter.getParameter().getName().equals(LOGIN_MEMBER)) {
+        if (parameter.getParameterType().equals(SessionMember.class) &&
+                parameter.getParameter().getName().equals(HANDLER_LOGIN_MEMBER_ARGUMENT_NAME)) {
             return true;
         }
         return false;
     }
 
     @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        SessionMember sessionMember = (SessionMember) webRequest.getAttribute(Constant.LOGIN_MEMBER, RequestAttributes.SCOPE_SESSION);
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        SessionMember sessionMember = (SessionMember) webRequest.getAttribute(Constant.LOGIN_MEMBER,
+                RequestAttributes.SCOPE_SESSION);
         if (sessionMember == null) {
-            throw new IllegalStateException("세션 내 로그인한 사용자 정보가 존재하지 않습니다.");
+            throw new IllegalStateException(NOT_EXISTED_LOGIN_MEMBER);
         }
-        Member storedMember = memberRepository.getById(sessionMember.getId());
-        return storedMember;
+        return sessionMember;
     }
 }
