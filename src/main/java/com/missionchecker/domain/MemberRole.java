0bcde6a16cf9a77aa739e600447d78235cb1513f
david <xodnjs2549@naver.com>
2022-07-19 16:45:38 +0900

    feat: 사용자 역할 추가

--- src/main/java/com/missionchecker/domain/MemberRole.java
+++ src/main/java/com/missionchecker/domain/MemberRole.java
@@ -1,5 +1,9 @@
 package com.missionchecker.domain;
 
 public enum MemberRole {
-    GUEST, APPLICANT, PARTICIPANT, ADMINISTRATOR
+    GUEST, // 로그인 하지 않은 사용자
+    APPLICANT, // 미션 신청자
+    PARTICIPANT, // 미션 참여자
+    ADMINISTRATOR, // 미션 관리자
+    OWNER // 미션 소유자(최고 관리자)
 }
